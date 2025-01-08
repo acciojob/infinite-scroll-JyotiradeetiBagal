7cb3d934ff838747bf43a99f1560f3d09b4a14b9
JyotiradeetiBagal <139766154+JyotiradeetiBagal@users.noreply.github.com>
2025-01-08 17:13:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here!
+document.addEventListener("DOMContentLoaded",()=>{
+    const list = document.getElementById("infi-list");
 
+    function addItems(count = 10) {
+        for(let i = 1; i <= count; i++) {
+            const listItem = document.createElement("li");
+            listItem.textContent = `Item ${list.children.length + 1}`;
+            list.appendChild(listItem);
+        }
+    }
+
+    addItems();
+
+    list.addEventListener("scroll", () => {
+        const { scrollTop, scrollHeight, clientHeight } = list;
+
+        if (scrollTop + clientHeight >= scrollHeight-5) {
+            addItems(2);
+        }
+    });
+});
